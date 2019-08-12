925a209159719a4aad10d7d715c3383d6efe3764
kkxingh <38274884+kkxingh@users.noreply.github.com>
2019-08-12 14:28:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+
+
+
+
+function submit_call(){
+    var show = document.getElementById("login-wow");
+
+    show.style.display='none';
+    if (show.style.display='none'){
+
+         document.getElementById("showme").style.display='block';
+        }else{
+            show.style.display='none';
+        }
+   
+
+
+
+
+
+
+
+
+
+}
+
+
+
+function colorc(){
+document.getElementById("section").style.backgroundColor ="black";
+
+var fix = document.getElementById("some");
+fix.style.display = 'none';
+var beta =document.getElementById("someus");
+beta.style.display='none'
+
+if (fix.style.display ='none', beta.style.display='none'){
+
+ document.getElementById("light").style.display='block';
+ document.getElementById("lightus").style.display="block";
+
+ 
+
+}else{
+
+    fix.style.display='none';
+    beta.style.display='none';
+
+}
+
+}
+
+function light(){
+
+    document.getElementById("section").style.backgroundColor ="#2135cc";
+    
+    var lix = document.getElementById("light");
+    lix.style.display = 'none';
+
+    var mix = document.getElementById("lightus")
+    mix.style.display='none';
+
+    if (lix.style.display ='none', mix.style.display='none'){
+
+        document.getElementById("some").style.display='block';
+        document.getElementById("someus").style.display="block";
+        
+        
+           
+
+       
+       }else{
+       
+           lix.style.display='none';
+           mix.style.display='none';
+       
+       }
+}
+
+function showme(){
+
+var logc = document.getElementById("showme");
+logc.style.display='none';
+
+
+if(logc.style.display='none'){
+
+     document.getElementById("login-wow").style.display='block';
+
+
+
+}else{
+    logc.style.display='none';
+}
+
+
+
+}
+
+
+  
+
